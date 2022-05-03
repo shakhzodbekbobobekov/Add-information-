d922048a98c343893767ad3928df16d08a1deb67
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-05-03 08:19:40 +0500

    add components files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [showItem, setShowItem] = useState(true);
+  const [events, setEvents] = useState([
+    { id: "1", day: "11.11.2001", fName: "Muhammadjon" },
+    { id: "2", day: "12.12.2002", fName: "Alijon" },
+    { id: "3", day: "10.10.2000", fName: "Abu Bakr" },
+  ]);
+
+  const removeBtn = (id) => {
+    setEvents(
+      events.filter((event) => {
+        return event.id !== id;
+      })
+    );
+  };
+
+  const showItemBtn = () => {
+    setShowItem(true);
+  };
+
+  const hideItemBtn = () => {
+    setShowItem(false);
+  };
+  return (
+    <div>
+      <h1>Happy birthday 🐱‍🏍</h1> <br />
+      <button onClick={showItemBtn}>Show item</button>
+      <button onClick={hideItemBtn}>Hide item</button>
+      {showItem && (
+        <>
+          {events.map((event) => {
+            return (
+              <div className="container" key={event.id}>
+                <h1>{event.fName}</h1>
+                <p>{event.day}</p>
+                <button onClick={() => removeBtn(event.id)}>O'chirish</button>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
